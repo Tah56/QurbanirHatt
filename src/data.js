83d16f7d7e77f5ed08bbf7f0f21e6af47bacc8b5
Tah56 <tanzimahmed575@gmail.com>
2026-05-05 02:48:07 +0600

    fix json data

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
  export const tip =async()=>{
-    const res = await fetch("http://localhost:3000/tips-data.json")
+    const res = await fetch("https://qurbanir-hatt.vercel.app/data.json")
     const data = await res.json()
     return data
 }
