cff7ecf944b43eca659671b34e7c8e465ba3c549
Tah56 <tanzimahmed575@gmail.com>
2026-05-05 18:08:50 +0600

    validation add

--- src/component/UpdateProfile/UpdateProfile.jsx
+++ src/component/UpdateProfile/UpdateProfile.jsx
@@ -1,31 +1,23 @@
 "use client";
 
-
 import { authClient } from "@/app/lib/auth-client";
-import {Button, Input, Label, Modal, Surface, TextField} from "@heroui/react";
+import { Button, Input, Label, Modal, Surface, TextField } from "@heroui/react";
 import { redirect } from "next/navigation";
 import { RxAvatar } from "react-icons/rx";
 import { toast } from "react-toastify";
 
 export function UpdateProfile() {
-
-     const onSubmit = async (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget); 
-    if(!formData){
-        toast.error("something wrong try again")
-        return
-    }
+    const formData = new FormData(e.currentTarget);
+
     const Userdata = Object.fromEntries(formData.entries());
     console.log(Userdata);
-    
+
     await authClient.updateUser({
-    image: Userdata.image,
-    name: Userdata.name
-})
- 
-    
-    
+      image: Userdata.image,
+      name: Userdata.name,
+    });
   };
   return (
     <Modal>
@@ -39,14 +31,25 @@ export function UpdateProfile() {
                 <RxAvatar className="size-5" />
               </Modal.Icon>
               <Modal.Heading>Update Info</Modal.Heading>
-              <p className="mt-1.5 text-sm leading-5 text-muted">
-               
-              </p>
+              <p className="mt-1.5 text-sm leading-5 text-muted"></p>
             </Modal.Header>
             <Modal.Body className="p-6">
               <Surface variant="default">
-                <form onSubmit={onSubmit} className="flex flex-col justify-between gap-4">
-                  <TextField className="w-full" name="name" type="text">
+                <form
+                  onSubmit={onSubmit}
+                  className="flex flex-col justify-between gap-4"
+                >
+                  <TextField
+                    className="w-full"
+                    name="name"
+                    type="text "
+                    validate={(value) => {
+                      if (value.length < 3) {
+                        return "Name must be at least 3 characters";
+                      }
+                      return null;
+                    }}
+                  >
                     <Label>Name</Label>
                     <Input placeholder="Enter your name" />
                   </TextField>
@@ -54,20 +57,24 @@ export function UpdateProfile() {
                     <Label>Email</Label>
                     <Input placeholder="Enter your image URL" />
                   </TextField>
-                 <div className="flex items-end justify-end gap-4 pt-10">
-
-              <Button slot="close" variant="secondary">
-                Cancel
-              </Button>
-              <Button type="submit" onClick={()=>{
-                  toast.success("profile update success")
-                }} slot="close">Save</Button>
-                </div>
+                  <div className="flex items-end justify-end gap-4 pt-10">
+                    <Button slot="close" variant="secondary">
+                      Cancel
+                    </Button>
+                    <Button
+                      type="submit"
+                      onClick={() => {
+                        toast.success("profile update success");
+                      }}
+                      slot="close"
+                    >
+                      Save
+                    </Button>
+                  </div>
                 </form>
               </Surface>
             </Modal.Body>
-            <Modal.Footer>
-            </Modal.Footer>
+            <Modal.Footer></Modal.Footer>
           </Modal.Dialog>
         </Modal.Container>
       </Modal.Backdrop>
