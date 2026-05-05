f3bcaf3ef13595f139abe8604309039dcd6e2bb2
Tah56 <tanzimahmed575@gmail.com>
2026-05-05 15:17:59 +0600

    add submit btn on form

--- src/component/UpdateProfile/UpdateProfile.jsx
+++ src/component/UpdateProfile/UpdateProfile.jsx
@@ -44,7 +44,7 @@ export function UpdateProfile() {
             </Modal.Header>
             <Modal.Body className="p-6">
               <Surface variant="default">
-                <form className="flex flex-col gap-4">
+                <form onSubmit={onSubmit} className="flex flex-col gap-4">
                   <TextField className="w-full" name="name" type="text">
                     <Label>Name</Label>
                     <Input placeholder="Enter your name" />
